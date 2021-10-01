f2d48a30ed8c356648c57af5522efcd82434cbda
Abderazak Erraknaoui <ABDERAZAK.ERRAKNAOUI@local.ittcesena.it>
2021-10-01 09:32:38 +0200

    Aggiunto ciclo per ricerca minima

--- MassimoMinimo/Program.cs
+++ MassimoMinimo/Program.cs
@@ -6,7 +6,18 @@ namespace MassimoMinimo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] array = new int[5] { 3, 6, -5, 4, 7 };
+
+            int minimo = array[0];
+
+            for(int i = 0; i < array.Length; i++)
+            {
+                if (array[i] < minimo)
+                    minimo = array[i];
+            }
+
+            Console.WriteLine(minimo);
+            Console.ReadKey();
         }
     }
 }
